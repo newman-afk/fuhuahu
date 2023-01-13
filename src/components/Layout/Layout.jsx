19a888ca0e3a61cbc7f7440593112f447a78517d
newman-afk <84593706+newman-afk@users.noreply.github.com>
2023-01-14 02:37:27 +0800

    navbar add

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import "./Layout.css";
 import { Outlet, Link } from "react-router-dom";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 const Layout = () => {
+  function sidebarToggle() {
+    const navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("navbar-mobile");
+  }
   return (
     <>
-      <Link to="/">Home</Link>
-      <Link to="/notes">Notes</Link>
+      <nav className="navbar container navbar-mobile">
+        <FontAwesomeIcon
+          className="navbar-open"
+          icon={faBars}
+          onClick={sidebarToggle}
+        ></FontAwesomeIcon>
+
+        <FontAwesomeIcon
+          className="navbar-close"
+          icon={faXmark}
+          onClick={sidebarToggle}
+        ></FontAwesomeIcon>
+
+        <div className="navbar-link-container">
+          <Link to="/">Home</Link>
+          <Link to="/notes">Notes</Link>
+        </div>
+      </nav>
+
       <Outlet />
     </>
   );
